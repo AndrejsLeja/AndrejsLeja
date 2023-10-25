017978430005f912f07f76714a829d17839b25c6
AndrejsLeja <144876819+AndrejsLeja@users.noreply.github.com>
2023-10-25 09:19:48 +0300

    Update result.py

--- result.py
+++ result.py
@@ -1,17 +1,15 @@
 from openpyxl import Workbook,load_workbook
-wb=load_workbook('data_files/')
+wb=load_workbook('data_files/EmployeeData.xlsl')
 ws=wb.active
 max_row = ws.max_row
 n = 0
 
 for row in range(2,max_row + 1):
-    id = ws['A'+str(row)].value
+    gender = ws['D'+str(row)].value
     rate = ws['B'+str(row)].value
     hours = ws['C'+str(row)].value
-    if(type(rate) != str and type(hours) != str):
-        salary = rate*hours
-        ws['D' + str(row)].value = salary
-    if(salary > 3000):
+    
+    if(gender == "male"):
         #print("People id " + str(id) + " His salary is " + str(salary))
         n=n+1
 print(n)
